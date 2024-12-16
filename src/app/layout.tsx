de2c0ddf19a5320afce3ef4ec684a2af9b66996f
MajidNjozi <abdulmajid.njozi@gmail.com>
2024-12-16 12:00:45 +0300

    local hosted opengraph images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,38 +7,38 @@ export const metadata: Metadata = {
   title: 'My Jamii - Community Engagement, Connections, and Learning',
   description:
     ' We understand living abroad calls for establishing connections with the local and the community of your fellow countrymen. ',
-  openGraph: {
-    type: 'website',
-    url: 'https://myjamii.com',
-    title: 'My Jamii - Community Engagement, Connections, and Learning',
-    description:
-      'We understand living abroad calls for establishing connections with the local and the community of your fellow countrymen.',
-    images: [
-      {
-        url: 'https://drive.google.com/uc?id=1N5Kzd8zb7UoQcnTEG2cCiP8hxig-nyjJ',
-        width: 1200,
-        height: 630,
-        alt: 'Rasello platform showcasing analytics, prediction, and communication tools',
-      },
-      {
-        url: 'https://drive.google.com/uc?id=1N5Kzd8zb7UoQcnTEG2cCiP8hxig-nyjJ',
-        width: 600,
-        height: 315,
-        alt: 'Rasello platform showcasing analytics, prediction, and communication tools',
-      },
-    ],
-  },
-  twitter: {
-    card: 'summary_large_image',
-    creator: '@myjamiiapp',
-    title: 'My Jamii - Community Engagement, Connections, and Learning',
-    description:
-      'We understand living abroad calls for establishing connections with the local and the community of your fellow countrymen.',
-    images: [
-      'https://drive.google.com/uc?id=1N5Kzd8zb7UoQcnTEG2cCiP8hxig-nyjJ',
-      'https://drive.google.com/uc?id=1N5Kzd8zb7UoQcnTEG2cCiP8hxig-nyjJ',
-    ],
-  },
+  // openGraph: {
+  //   type: 'website',
+  //   url: 'https://myjamii.com',
+  //   title: 'My Jamii - Community Engagement, Connections, and Learning',
+  //   description:
+  //     'We understand living abroad calls for establishing connections with the local and the community of your fellow countrymen.',
+  //   images: [
+  //     {
+  //       url: 'https://drive.google.com/uc?id=1N5Kzd8zb7UoQcnTEG2cCiP8hxig-nyjJ',
+  //       width: 1200,
+  //       height: 630,
+  //       alt: 'Rasello platform showcasing analytics, prediction, and communication tools',
+  //     },
+  //     {
+  //       url: 'https://drive.google.com/uc?id=1N5Kzd8zb7UoQcnTEG2cCiP8hxig-nyjJ',
+  //       width: 600,
+  //       height: 315,
+  //       alt: 'Rasello platform showcasing analytics, prediction, and communication tools',
+  //     },
+  //   ],
+  // },
+  // twitter: {
+  //   card: 'summary_large_image',
+  //   creator: '@myjamiiapp',
+  //   title: 'My Jamii - Community Engagement, Connections, and Learning',
+  //   description:
+  //     'We understand living abroad calls for establishing connections with the local and the community of your fellow countrymen.',
+  //   images: [
+  //     'https://drive.google.com/uc?id=1N5Kzd8zb7UoQcnTEG2cCiP8hxig-nyjJ',
+  //     'https://drive.google.com/uc?id=1N5Kzd8zb7UoQcnTEG2cCiP8hxig-nyjJ',
+  //   ],
+  // },
 };
 
 export default function RootLayout({
